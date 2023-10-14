6ddf44f4c7fdf545e3833a0f6b6e7c0616f8a6c5
greicyellem <greicysantana1@gmail.com>
2023-10-14 13:23:03 -0300

    ok

--- src/themes.jsx
+++ src/themes.jsx
@@ -1,7 +1,7 @@
 export const lightTheme = {
-    body: '#909dc0',
+    body: 'white',
     text: '#37415d',
-    header: '#787a84'
+    header: 'white'
 
 }
 
@@ -10,21 +10,4 @@ export const darkTheme = {
     text: '#FAFAFA',
     header: '#586077'
 
-}
-
-export default {
-    COLORS: {
-        BACKGROUND_900: "#232129", 
-        BACKGROUND_800: "#312E38", 
-        BACKGROUND_700: "#3E3D47", 
-
-        WHITE: "#F4EDE8",
-        ORANGE: "#FF9000",
-        
-        GRAY_100: "#999591",
-        GRAY_300: "#666360",
-
-        RED: "#FF002E",
-
-    }
 };
